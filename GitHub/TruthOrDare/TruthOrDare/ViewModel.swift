9d44d2ef7971b52f3881ff7f0bb22b8eb832d20d
Shiv Kalola <shivkalola@gmail.com>
2019-07-22 13:24:59 -0400

    Add comment truths with carthage fix

--- GitHub/TruthOrDare/TruthOrDare/ViewModel.swift
+++ GitHub/TruthOrDare/TruthOrDare/ViewModel.swift
@@ -14,7 +14,7 @@ class ViewModel {
     private let truths = [
         "What are you most self-conscious about?",
         "What would you do if you were the opposite sex for a month?",
-        "What is the most expensive thing you stole?",
+        "What is the cheapest thing you stole?",
         "What is the most childish thing you still do?",
         "Have you ever let someone take the blame for something you did?"
     ]
