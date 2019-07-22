4dd042932969c10f4fc235143c3ac0bd55789321
Shiv Kalola <shivkalola@gmail.com>
2019-07-22 14:09:52 -0400

    test carthage fix6

--- GitHub/TruthOrDare/TruthOrDare/ViewModel.swift
+++ GitHub/TruthOrDare/TruthOrDare/ViewModel.swift
@@ -13,10 +13,10 @@ class ViewModel {
     //Truth
     private let truths = [
         "What are you most self-conscious about?",
-        "What would you do if you for a month?",
-        "What is the cheapest thing you stole?",
+        "What would you do if you were the sex for a month?",
+        "What is the most expensive thing you have stolen?",
         "What is the most childish thing you still do?",
-        "Have you ever let someone take the blame for you?"
+        "Have you ever let someone take the blame for something you did?"
     ]
     
     //Dare
